a606e647f569727eb923bfaebba6540533159121
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-31 20:55:28 -0300

    first commit

---
+++ Consumidor.Mensageria/Consumidor.Mensageria/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost"
+
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+const string fila = "fila_teste";
+
+
+channel.QueueDeclare(queue: fila,
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, args) =>
+{
+    var body = args.Body.ToArray();
+    var mensagem = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Mensagem recebida: {mensagem}...");
+};
+
+channel.BasicConsume(queue: fila,
+                     autoAck: true,
+                     consumer: consumer);
+
+Console.WriteLine("Aguardando mensagem");
+Console.ReadLine();
---
+++ Produtor.Mensageria/Produtor.Mensageria/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost"
+
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+const string fila = "fila_teste";
+
+
+channel.QueueDeclare(queue: fila,
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+
+int contador = 0;
+while (contador <10 )
+{
+    string mensagem = $"Mensagem: {contador}.";
+    var body = Encoding.UTF8.GetBytes(mensagem);
+
+    channel.BasicPublish(exchange: "",
+                        routingKey: fila,
+                        basicProperties: null,
+                        body: body);
+
+    Thread.Sleep(3000);
+    contador++;
+
+
+}
+
+Console.WriteLine("Mensagem Postada com sucesso!");
+Console.ReadLine();
